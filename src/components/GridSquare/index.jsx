efc7997c45157c7b2d3a1c733ced4b1b4554ae74
BrittGall <crlnfllr@gmail.com>
2023-10-24 23:44:56 -0500

    out of stock refactored

--- src/components/GridSquare/index.jsx
+++ src/components/GridSquare/index.jsx
@@ -1,23 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 import { Link } from 'gatsby';
 import Img from 'gatsby-image';
 
 export default function GridSquare({id, slug, image, title, price}) {
-  const addLink = () =>{
+  const [isOutOfStock, setIsOutOfStock] = useState(false);
+  const reDirect = () =>{
       window.location.href =`../../products/${slug}`; 
   }
+  const options = {
+    method: 'GET',
+    url: `https://app.snipcart.com/api/products/${id}-en`,
+    params: {limit: '1'},
+    headers: {
+      Accept: 'application/json',
+      Authorization: `Basic ${process.env.GATSBY_API_AUTH}`,
+      'content-type': 'application/json'
+    }
+  };
+  axios.request(options).then(function (response) {
+    const items = response.data;
+    const stock = items.stock;
+    
+    if (stock === 0){
+      setIsOutOfStock(true);
+    } else {
+      setIsOutOfStock(false);
+    }
+
+  }).catch(function (error) {
+    console.error(error);
+  });
   return (
   <div className='product-item'>
     <div className='product-square'>
-          <div id={`${id}`} className='hide'><p>Out of Stock</p></div>
-          
+          {isOutOfStock && (<div className='soldOut' id={`${id}`}><p>Out of Stock</p></div>)}
           <Link to={`../../products/${slug}`} tabIndex='-1'>
           <Img
             objectFit='cover'
             fluid={image.fluid}
             alt={title}
             />
-            <div className='overlay text' onKeyUp={addLink} tabIndex='0'>
+            <div className='overlay text' onKeyUp={reDirect} tabIndex='0'>
               <h2>{title}</h2>    
               <h3>${price}</h3>
             </div>
